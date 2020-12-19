b0da476346cd96429d3a53a3ceae7a4065a82c1c
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2020-12-19 15:37:46 +0500

    padding

--- Welcome-Login-Signup-Page-Flutter-master/lib/Screens/LandLordDashbord/components/add_post/addpost.dart
+++ Welcome-Login-Signup-Page-Flutter-master/lib/Screens/LandLordDashbord/components/add_post/addpost.dart
@@ -133,31 +133,7 @@ class AddPost extends StatelessWidget {
               },
             ),
 
-            //dropdown
-            // DropdownButton<String>(
-            //   value: dropdownValue,
-            //   icon: Icon(Icons.arrow_downward),
-            //   iconSize: 24,
-            //   elevation: 16,
-            //   style: TextStyle(color: Colors.deepPurple),
-            //   underline: Container(
-            //     height: 2,
-            //     color: Colors.deepPurpleAccent,
-            //   ),
-            //   onChanged: (String newValue) {
-            //     // setState(() {
-            //     //   dropdownValue = newValue;
-            //     // });
-            //   },
-            //   items: <String>['One', 'Two', 'Free', 'Four']
-            //       .map<DropdownMenuItem<String>>((String value) {
-            //     return DropdownMenuItem<String>(
-            //       value: value,
-            //       child: Text(value),
-            //     );
-            //   }).toList(),
-            // ),
-           //izedBox(height: size.height * 0.04),
+
             RoundedButton(
               text: "SUBMIT",
               press: () {
--- Welcome-Login-Signup-Page-Flutter-master/lib/components/rounded_price.dart
+++ Welcome-Login-Signup-Page-Flutter-master/lib/components/rounded_price.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/components/text_field_container.dart';
 import 'package:flutter_auth/constants.dart';
-
+import 'package:badges/badges.dart';
 class RoundedPrice extends StatelessWidget {
   final String hintText;
-  final IconData icon;
+
   final ValueChanged<String> onChanged;
   const RoundedPrice ({
     Key key,
     this.hintText,
-    this.icon = Icons.settings,
+    //this.icon = Icons.dollars,
     this.onChanged,
   }) : super(key: key);
 
@@ -20,10 +20,7 @@ class RoundedPrice extends StatelessWidget {
         onChanged: onChanged,
         cursorColor: kPrimaryColor,
         decoration: InputDecoration(
-          icon: Icon(
-            icon,
-            color: kPrimaryColor,
-          ),
+          
           hintText: hintText,
           border: InputBorder.none,
         ),

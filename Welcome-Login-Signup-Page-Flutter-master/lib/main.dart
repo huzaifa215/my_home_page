2993ebfada08189703ecdac047bffa5cd5975ff1
huzaifa215 <66369515+huzaifa215@users.noreply.github.com>
2021-01-02 13:50:37 +0500

    OTP

--- Welcome-Login-Signup-Page-Flutter-master/lib/main.dart
+++ Welcome-Login-Signup-Page-Flutter-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/RunOtpView.dart';
 import 'package:flutter_auth/Screens/LandLordDashbord/landlorddash.dart';
 import 'package:flutter_auth/Screens/TenantHomePage/TenantHomePage.dart';
 import 'package:flutter_auth/buttombar.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_auth/calculator.dart';
 import 'package:flutter_auth/constants.dart';
 import 'package:splashscreen/splashscreen.dart';
 
+import 'Screens/LandLordDashbord/components/padding_posts/view_property.dart';
 import 'Screens/Welcome/welcome_screen.dart';
 import 'calculatorRun.dart';
 
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home:WelcomeScreen(),
+      home:RunOptView(),
     );
   }
 }
